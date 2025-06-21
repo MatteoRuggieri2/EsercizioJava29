a2d6aa1c66f74540dc0089624cd294348a418981
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-06-21 19:34:05 +0200

    run() method done

--- src/esercizi/FileWriterPrintDemo.java
+++ src/esercizi/FileWriterPrintDemo.java
@@ -1,10 +1,39 @@
 package esercizi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class FileWriterPrintDemo {
+	
+	final private String fileName = "src/text_files/print_writer_output.txt";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new FileWriterPrintDemo().run();
+	}
+	
+	private void run() {
+		
+		File f = new File(fileName);
+		
+		try {
+			PrintWriter pw = new PrintWriter(f);
+			pw.print("string-1\n");
+			pw.print("string-2\n");
+			pw.println("this-is-period-1");
+			pw.println("this-is-period-2");
+			
+			// Flush and close
+			pw.flush();
+			pw.close();
+			
+			System.out.println("Processo terminato con successo.");
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("Il file con path \"" + fileName + "\" non esiste.");
+			e.printStackTrace();
+		}
+		
 	}
 
 }
